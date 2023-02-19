d5fcc88cf1e40652f15ae491ed933e9a51c8fe7d
Máté Horváth <BAD50U@tr.pte.hu>
2023-02-19 17:31:14 +0100

    Updated project page with dynamic image handling, and lightbox.

--- src/components/pages/ProjectPage.jsx
+++ src/components/pages/ProjectPage.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import PostImage from "../PostImage";
 import projects from "../../projects";
 import { useParams } from "react-router";
+import ModalImage from "react-modal-image";
 
 export default function ProjectPage(){
 
     const { pageUrl } = useParams();
     const project = projects.find((p) => p.pageUrl === pageUrl);
     const imageClass = "project-image"
+
+    const IMG = (imgName) => {
+        return require('../../img/'+ imgName)
+    }
 
 
     if(!project) {
@@ -17,10 +22,15 @@ export default function ProjectPage(){
     return(
         <div className="project-container container-fluid">
             <div>
-                <PostImage src={project.imageUrl} class={imageClass}/>
-                <PostImage src={project.imageUrl} class={imageClass}/>
-                <PostImage src={project.imageUrl} class={imageClass}/>
-                <PostImage src={project.imageUrl} class={imageClass}/>
+                {project.imagePortfolio.map((image, index) => (
+                    <ModalImage 
+                    small={IMG(project.imagePortfolio[index])}
+                    large={IMG(project.imagePortfolio[index])}
+                    alt={project.name}
+                    className={imageClass}
+                    hideDownload= "true"
+                />
+                ))}
             </div>
             <div className="project-content">
                <h1>{project.name}</h1>
